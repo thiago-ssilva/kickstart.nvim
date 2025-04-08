b46cbecc4a2856f8d0a142a017a3560e3434e9e0
Thiago Silva <thiago.silva@avenuecode.com>
2025-04-08 17:26:43 -0300

    add bufferline

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -71,4 +71,41 @@ return {
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  -- Bufferline
+  {
+    'akinsho/bufferline.nvim',
+    event = 'VeryLazy',
+    keys = {
+      --   { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      --   { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      --   { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      --   { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+      { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+      --   { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      --   { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      --   { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+      --   { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    },
+    config = function(_, opts)
+      require('bufferline').setup(opts)
+    end,
+    opts = {
+      options = {
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = {
+            Error = ' ',
+            Warn = ' ',
+            -- Info  = " ",
+            -- Hint  = " ",
+          }
+
+          local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
+          return vim.trim(ret)
+        end,
+        diagnostics = 'nvim_lsp',
+      },
+    },
+  },
 }
