b1d7cb784b2502c7f42ada5fdcdc4ef333dcbf9b
Thiago Silva <thiago.silva@avenuecode.com>
2025-04-08 16:32:47 -0300

    change lsp to load on buff events

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -16,6 +16,7 @@ return {
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
